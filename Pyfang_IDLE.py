d615d7ec2f3a3207eec47f46694275e52c6fb693
Pyfang <2950966704@qq.com>
2020-12-13 16:03:30 +0800

    Add an update

--- Pyfang_IDLE.py
+++ Pyfang_IDLE.py
@@ -1,6 +1,6 @@
-import time
 import easygui as es
 import turtle as tu
+import tkinter as tk
 bl = []
 bls = []
 def valuecheck(checks):
@@ -43,38 +43,58 @@ def pyfangrun(command):
                 bl.append(splited[0])
                 bls.append(valuecheck(splited[2]))
         elif splited[0] == 'print':
-            print(valuecheck(splited[1]))
+            t1.color('green')
+            t1.write(valuecheck(splited[1]),font=('arial',12))
+            t1.setheading(-90)
+            t1.penup()
+            t1.fd(24)
         elif splited[0] == 'fd':
-            tu.fd(int(splited[1]))
+            t2.fd(int(splited[1]))
         elif splited[0] == 'back':
-            tu.back(int(splited[1]))
+            t2.back(int(splited[1]))
         elif splited[0] == 'head':
-            tu.setheading(int(splited[1]))
+            t2.setheading(int(splited[1]))
         elif splited[0] == 'color':
-            tu.pencolor(splited[1])
+            t2.pencolor(splited[1])
         elif splited[0] == 'dot':
-            tu.dot(int(splited[1]))
+            t2.dot(int(splited[1]))
         elif splited[0] == 'size':
-            tu.pensize(int(splited[1]))
+            t2.pensize(int(splited[1]))
         elif splited[0] == 'right':
-            tu.right(int(splited[1]))
+            t2.right(int(splited[1]))
         elif splited[0] == 'left':
-            tu.left(int(splited[1]))
+            t2.left(int(splited[1]))
         elif splited[0] == 'goto':
-            tu.goto(int(splited[1]),int(splited[2]))
+            t2.goto(int(splited[1]),int(splited[2]))
         elif splited[0] == 'msgbox':
             es.msgbox(valuecheck(splited[1]),valuecheck(splited[2]),valuecheck(splited[3]))
         else:
             es.msgbox("NameError: name "+splited[0]+"' is not defined",'Error')
     elif splited[0] == 'down':
-        tu.pendown()
+        t2.pendown()
     elif splited[0] == 'up':
-        tu.penup()
+        t2.penup()
     elif splited[0] == 'clear':
-        tu.clear()
+        t2.clear()
     else:
         es.msgbox("NameError: name "+splited[0]+"' is not defined",'Error')
-print('''
+cmd = []
+r = tu._Root()
+r.set_geometry(800,800,0,0)
+cv2 = tu.ScrolledCanvas(r)
+cv2.pack(expand = True, fill = tk.BOTH)
+cv2.reset(400, 4000)
+t1 = tu.RawPen(cv2)
+cv3 = tu.ScrolledCanvas(r)
+cv3.pack(expand = True, fill = tk.BOTH)
+cv3.reset(400, 400)
+t2 = tu.RawPen(cv3)
+t2.shape('turtle')
+t1.hideturtle()
+t1.penup()
+t1.goto(-350,70)
+t1.color('green')
+t1.write('''
   _____        __                  
  |  __ \      / _|                 
  | |__) |   _| |_ __ _ _ __   __ _ 
@@ -83,12 +103,18 @@ print('''
  |_|    \__, |_| \__,_|_| |_|\__, |
          __/ |                __/ |
         |___/                |___/ 
-''')
-print('''
-Pyfang IDLE 2.9 Copyright by Fangcat(Fang.Zixian)
-Using packages:Turtle,Easygui,Time
-Loading....
-''')
-time.sleep(2)
+Pyfang 2.9 IDLE Copyright by Fangcat(Fang.Zixian)
+Using packages:Turtle,Easygui
+''',font=('arial',12))
+t1.setheading(-90)
+t1.penup()
+t1.fd(24)
+t1.color('purple')
 while True:
-    pyfangrun(es.enterbox('Codebox','Codebox'))
+    a = es.enterbox('>>>','Pyfang')
+    t1.color('purple')
+    t1.write('>>>'+a,font=('arial',12))
+    t1.setheading(-90)
+    t1.penup()
+    t1.fd(24)
+    pyfangrun(a)
